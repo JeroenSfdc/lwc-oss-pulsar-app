2072588a46e58130505199b5fa8143eb9abf9a53
JeroenSfdc <jburgers@salesforce.com>
2021-06-04 00:07:07 +0200

    Commit

---
+++ src/client/modules/my/app/app.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+export default class App extends LightningElement {
+
+    connectedCallback() {
+        console.log('Hi there, it is me again...!');
+    }
+
+}
